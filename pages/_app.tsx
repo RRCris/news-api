121545a8e6822a74dbacd1bad36651c774a86a8e
RRCRis <curiosity.cdrr@gmail.com>
2022-09-28 20:15:49 -0500

    subir boilerpalce

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>News App</title>
+        <meta name="nueva app de noticias" content="noticias del mundo" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header id="header">news api🍀</header>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
